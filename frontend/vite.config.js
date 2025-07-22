5ab95eda20c7a706e8f76727e09aa072074a7d32
江韬 <tianjiuweixun@gmail.com>
2025-07-22 16:19:48 +0800

    修改

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/my-vue-sqlite-app/', // <-- 确保 'my-vue-sqlite-app' 是你的 GitHub 仓库名
+   base: '/', // <-- 确保 'my-vue-sqlite-app' 是你的 GitHub 仓库名
   plugins: [
     vue(),
     vueDevTools(),
